0b8b83d2f9ca9d7055c0e6540bf28f0bd0b679c1
Gaurav-hzb <144571344+Gaurav-hzb@users.noreply.github.com>
2023-10-19 22:34:14 +0530

    Week -1  Assignment 2 Solution filles are added

---
+++ Week 1 - 2nd Assignment/VolumeOfCylinder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius,height;
+    cout<<"Enter the radius of the cylinder  ";
+    cin>>radius;
+     cout<<"Enter the Hieght of the cylinder  ";
+    cin>>height;
+   cout<<"the volume of cylinder is:  "<<3.14*radius*radius*height;
+}
